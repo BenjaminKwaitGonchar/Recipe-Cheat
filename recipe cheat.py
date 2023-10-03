90475a55fcad48a8c928d540dfaa7ccafde5fe51
BenjaminKwaitGonchar <bk62@rice.edu>
2023-10-03 18:37:22 -0500

    Functions partially completed

--- recipe cheat.py
+++ recipe cheat.py
@@ -4,10 +4,22 @@ with open("test text.txt") as f:
         lines[line_num] = lines[line_num].strip("\n")
     f.close()
 
+def range_converter(line):
+    range_str = line.partition("range(")[1] + line.partition("range(")[2].partition(")")[0] + line.partition("range(")[2].partition(")")[1]
+    realized_range = eval(range_str)
+    recipeized_range = "["
+    recipeized_range += (str(realized_range[0]) + ", " + str(realized_range[1]) + ", ... , " + str(realized_range[-1]) + "]")
+    return recipeized_range
+
 new_lines = []
 
 for line in lines:
-    line = line.replace("=","$$<--$$")
+    line = line.replace("=","<--")
+    line = line.replace("**","^")
+    line = line.replace("%","mod")
+    line = line.replace('"""',"")
+    if "import" in line:
+        line = ""
     if "for" in line:
         line = line.replace(":"," do")
         line = line.replace("for","for each")
@@ -16,10 +28,15 @@ for line in lines:
         line = line = line.replace("==","=")
         line = line = line.replace(">=","$$\geq$$")
         line = line = line.replace(">=","$$\leq$$")
+    if "def" in line:
+        line = line.replace("def","Name:")
+        line = line[:line.find("(")]
+    if "range(" in line:
+        line = line.replace(line.partition("range(")[1] + line.partition("range(")[2].partition(")")[0] + line.partition("range(")[2].partition(")")[1], range_converter(line))
     
     new_lines.append(line)
 
 for line in new_lines:
     print(line)
 
-def range_converter()
+#print(range_converter(lines[3]))
