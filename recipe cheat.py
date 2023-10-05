97ebe684f5c0a8a25a57aae4b5725d236ba8a5d5
BenjaminKwaitGonchar <bk62@rice.edu>
2023-10-04 22:41:13 -0500

    nfhf

--- recipe cheat.py
+++ recipe cheat.py
@@ -1,19 +1,20 @@
+import docx
+
+recipe = docx.Document()
+newlines_viz = []
 with open("test text.txt") as f:
     lines = f.readlines()
-    for line_num in range(len(lines)):
-        lines[line_num] = lines[line_num].strip("\n")
     f.close()
 
 def range_converter(line):
     range_str = line.partition("range(")[1] + line.partition("range(")[2].partition(")")[0] + line.partition("range(")[2].partition(")")[1]
     realized_range = eval(range_str)
-    recipeized_range = "["
-    recipeized_range += (str(realized_range[0]) + ", " + str(realized_range[1]) + ", ... , " + str(realized_range[-1]) + "]")
+    recipeized_range = ""
+    recipeized_range += (str(realized_range[0]) + ", " + str(realized_range[1]) + ", ... , " + str(realized_range[-1]) + "")
     return recipeized_range
 
-new_lines = []
-
 for line in lines:
+    line = line.strip("\n")
     line = line.replace("=","<--")
     line = line.replace("**","^")
     line = line.replace("%","mod")
@@ -26,17 +27,16 @@ for line in lines:
     elif "if" in line:
         line = line.replace(":"," then")
         line = line = line.replace("==","=")
-        line = line = line.replace(">=","$$\geq$$")
-        line = line = line.replace(">=","$$\leq$$")
+        line = line = line.replace(">=","≥")
+        line = line = line.replace("<=","≤")
     if "def" in line:
         line = line.replace("def","Name:")
         line = line[:line.find("(")]
     if "range(" in line:
         line = line.replace(line.partition("range(")[1] + line.partition("range(")[2].partition(")")[0] + line.partition("range(")[2].partition(")")[1], range_converter(line))
-    
-    new_lines.append(line)
+    if line != '' and line.isspace() == False:
+        print(line)
+        recipe.add_paragraph(text=line, style=None)
+        newlines_viz.append(line)
 
-for line in new_lines:
-    print(line)
-
-#print(range_converter(lines[3]))
+recipe.save('test output.docx')
